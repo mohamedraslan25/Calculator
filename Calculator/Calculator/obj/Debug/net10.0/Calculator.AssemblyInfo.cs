f230f2d8c2b9d35f8121835ae62553ab708c4ff2
moh7amedayman25 <moh7amedaymanraslan@gmail.com>
2026-09-11 08:19:16 +0300

    feat: addming a choosing continue or not

--- Calculator/Calculator/obj/Debug/net10.0/Calculator.AssemblyInfo.cs
+++ Calculator/Calculator/obj/Debug/net10.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2469e7cf9ba9257145f683d12963e902519dd3c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41a3319de9807f82226b5b7e4968b675b75b56d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
